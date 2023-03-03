95f51dc22c54048bb58911db46ec4bf37262ce56
Paul Launay <launay.p@gmail.com>
2023-03-03 17:46:10 +0100

    feat: downgrade packages and test config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { InjectedConnector, StarknetConfig } from "@starknet-react/core";
-import ControllerConnector from "@cartridge/connector";
+import CartridgeConnector from "@cartridge/connector";
+import { RpcProvider } from "starknet";
 
 import "@/styles/globals.css";
 
@@ -9,13 +10,25 @@ import type { AppProps } from "next/app";
 const connectors = [
   new InjectedConnector({ options: { id: "braavos" } }),
   new InjectedConnector({ options: { id: "argentX" } }),
-  new ControllerConnector(),
+  // new CartridgeConnector(),
+  new CartridgeConnector([], {
+    // url: "http://localhost:3000",
+    url: "https://x.cartridge.gg",
+  }),
 ];
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <StarknetConfig connectors={connectors} autoConnect>
+      <StarknetConfig
+        autoConnect
+        connectors={connectors}
+        defaultProvider={
+          new RpcProvider({
+            nodeUrl: "https://starknet-goerli.cartridge.gg/rpc/v0.2",
+          })
+        }
+      >
         <Component {...pageProps} />
       </StarknetConfig>
     </ChakraProvider>
